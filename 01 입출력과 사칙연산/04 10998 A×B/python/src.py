d09bf043a68907911cd8dbfb2e51a81a406b10bf
Khamax <khamax4mr@gmail.com>
2023-02-07 09:50:19 +0000

    10998 python 코드, 예제, 경로, readme 추가.

---
+++ 01 입출력과 사칙연산/04 10998 A×B/python/src.py
@@ -0,0 +1,12 @@
+from sys import exit
+
+# 숫자 a, b 입력.
+inputs = input().split()
+a = int(inputs[0])
+b = int(inputs[1])
+
+if a <= 0 or a >= 10: exit(0)
+if b <= 0 or b >= 10: exit(0)
+
+# 출력
+print(a * b)
