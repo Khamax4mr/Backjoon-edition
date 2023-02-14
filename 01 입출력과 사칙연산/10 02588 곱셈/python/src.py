182bc4f38858d341216ffbcbda82b3cc455b9670
Khamax <khamax4mr@gmail.com>
2023-02-14 13:38:46 +0000

    2588 python 코드, 예제, 경로, readme 추가.

---
+++ 01 입출력과 사칙연산/10 02588 곱셈/python/src.py
@@ -0,0 +1,14 @@
+from sys import exit
+
+# 숫자 a, b 입력.
+a = int(input())
+b = int(input())
+
+if a < 100 or a >= 1000: exit()
+if b < 100 or b >= 1000: exit()
+
+# 출력.
+print(a * (b % 10))
+print(a * (b % 100 // 10))
+print(a * (b % 1000 // 100))
+print(a * b)
