84780e1e9db1ace593ef8424765cabd5adcc394a
Khamax <khamax4mr@gmail.com>
2023-02-20 07:55:22 +0000

    09498 python 코드, 예제, 경로, readme 추가.

---
+++ 02 조건문/02 09498 시험 성적/python/src.py
@@ -0,0 +1,14 @@
+from sys import exit
+
+# 점수 score 입력.
+inputs = input().split()
+score = int(inputs[0])
+
+if score < 0 or score > 100: exit()
+
+# 출력.
+if score >= 90: print('A')
+elif score >= 80: print('B')
+elif score >= 70: print('C')
+elif score >= 60: print('D')
+else: print('F')
