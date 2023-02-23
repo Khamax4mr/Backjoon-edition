37ca472f9bd18758eb88bce5efb1d8ba41dd738b
Khamax <khamax4mr@gmail.com>
2023-02-23 13:57:44 +0000

    02884 python 코드, 예제, 경로, readme 추가.

---
+++ 02 조건문/05 02884 알람 시계/README.md
@@ -0,0 +1,9 @@
+# 알람 시계
+
+## 공통
+* [조건문 (conditional)](https://github.com/Khamax4mr/Backjoon-edition/wiki/%EC%A1%B0%EA%B1%B4%EB%AC%B8-(conditional))
+
+***
+
+## 파이썬
+* none
---
+++ 02 조건문/05 02884 알람 시계/python/src.py
@@ -0,0 +1,14 @@
+from sys import exit
+
+# 시간 h, m 입력.
+h, m = map(int, input().split())
+if h < 0 or h > 23: exit(0)
+if m < 0 or h > 59: exit(0)
+
+# 시간 계산
+if m < 45:
+    h = (h + 23) % 24
+m = (m + 15) % 60
+
+# 출력.
+print(h, m)
