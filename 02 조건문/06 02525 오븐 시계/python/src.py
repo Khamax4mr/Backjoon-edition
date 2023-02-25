3de1a2a47fcedf3f6366dab507b6a60484bc1e76
Khamax <khamax4mr@gmail.com>
2023-02-25 00:40:49 +0000

    02525 python 코드, 예제, 경로, readme 추가.

---
+++ 02 조건문/06 02525 오븐 시계/python/src.py
@@ -0,0 +1,18 @@
+from sys import exit
+
+# 시간 a, b, c 입력.
+a, b = map(int, input().split())
+c = int(input())
+if a < 0 or a > 23: exit(0)
+if b < 0 or b > 59: exit(0)
+if c < 0 or c > 1000: exit(0)
+
+# 시간 계산.
+cooking_h = c // 60
+cooking_m = c % 60
+carry = (b + cooking_m) // 60
+b = (b + cooking_m) % 60
+a = (a + cooking_h + carry) % 24
+
+# 출력.
+print(a, b)
