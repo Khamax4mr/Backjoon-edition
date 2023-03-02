a7e8adf859401c2729ea2ed0b7c603dc4a4be10b
Khamax <khamax4mr@gmail.com>
2023-03-02 12:51:41 +0000

    10950 python 코드, 예제, 경로, readme 추가.

---
+++ 03 반복문/02 10950 A+B -3/python/src.py
@@ -0,0 +1,14 @@
+from sys import exit
+
+# 테스트케이스 개수 n 입력.
+n = int(input())
+if n < 0: exit(0)
+
+for i in range(n):
+    # 숫자 a, b 입력.
+    a, b = map(int, input().split())
+    if a <= 0 or a >= 10: exit(0)
+    if b <= 0 or b >= 10: exit(0)
+
+    # 출력.
+    print(a + b)
