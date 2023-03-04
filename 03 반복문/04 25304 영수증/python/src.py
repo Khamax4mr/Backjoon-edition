cb9a3ee8724f867a9397f5e4050914ee9241c51f
Khamax <khamax4mr@gmail.com>
2023-03-04 02:41:49 +0000

    25304 python 코드, 예제, 경로, readme 추가

---
+++ 03 반복문/04 25304 영수증/python/src.py
@@ -0,0 +1,23 @@
+from sys import exit
+
+# 총 금액 x 입력.
+x = int(input())
+if x < 1 or x > 1000000000: exit(0)
+
+# 구매 물건 종류 개수 n 입력.
+n = int(input())
+if n < 1 or n > 100: exit(0)
+
+# 영수증 총 금액 합산.
+sum = 0
+for i in range(n):
+    # 물건의 가격, 금액 a, b 입력.
+    a, b = map(int, input().split())
+    if a < 1 or a > 1000000: exit(0)
+    if b < 1 or b > 10: exit(0)
+
+    # 물건 구매 금액 추가.
+    sum += a * b
+
+# 출력.
+print("Yes" if x == sum else "No")
