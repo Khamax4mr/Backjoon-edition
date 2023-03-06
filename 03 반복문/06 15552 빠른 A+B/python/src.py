e782a123d82c6fe3cbb2bf1a6b9d5f43965de58f
Khamax <khamax4mr@gmail.com>
2023-03-06 03:12:15 +0000

    15552 python 코드, 예제, 경로, readme 추가.

---
+++ 03 반복문/06 15552 빠른 A+B/python/src.py
@@ -0,0 +1,14 @@
+from sys import exit, stdin
+
+# 테스트케이스 개수 t 입력.
+t = int(stdin.readline().rstrip())
+if t < 0 or t > 1000000: exit(0)
+
+for i in range(t):
+    # 숫자 a, b 입력.
+    a, b = map(int, stdin.readline().rstrip().split())
+    if a < 1 or a > 1000: exit(0)
+    if b < 1 or b > 1000: exit(0)
+
+    # 출력.
+    print(a + b)
