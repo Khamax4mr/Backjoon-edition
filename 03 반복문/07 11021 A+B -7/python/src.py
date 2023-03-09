e655e655880bf270689770edfdd000c2fbfc924a
Khamax <khamax4mr@gmail.com>
2023-03-09 10:38:59 +0000

    11021 python 코드, 예제, 경로, readme 추가.

---
+++ 03 반복문/07 11021 A+B -7/python/src.py
@@ -0,0 +1,14 @@
+from sys import exit
+
+# 테스트케이스 개수 t 입력.
+t = int(input())
+if t < 0: exit(0)
+
+for i in range(1, t+1):
+    # 숫자 a, b 입력.
+    a, b = map(int, input().split())
+    if a <= 0 or a >= 10: exit(0)
+    if b <= 0 or b >= 10: exit(0)
+
+    # 출력.
+    print('Case #' + str(i) + ':', (a + b))
