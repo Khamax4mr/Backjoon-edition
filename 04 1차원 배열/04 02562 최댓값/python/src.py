233ca58f2d1bce1cec9f0905840080adebcb1f8c
Khamax <khamax4mr@gmail.com>
2023-03-20 03:22:04 +0000

    02562 python 코드, 예제, 경로, readme 추가.

---
+++ 04 1차원 배열/04 02562 최댓값/python/src.py
@@ -0,0 +1,10 @@
+from sys import exit
+
+# 배열 numbers 입력.
+numbers = [int(input()) for i in range(9)]
+for elem in numbers:
+    if elem < 1 or elem > 100: exit(0)
+
+# 출력.
+print(max(numbers))
+print(numbers.index(max(numbers)) + 1)
