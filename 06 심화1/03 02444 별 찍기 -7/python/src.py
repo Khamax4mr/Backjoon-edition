5936e71263f99fdebdf486c161c1f09dd902cefb
Khamax <khamax4mr@gmail.com>
2023-04-18 03:53:22 +0000

    02444 python 코드, 예제, 경로, readme 추가.

---
+++ 06 심화1/03 02444 별 찍기 -7/python/src.py
@@ -0,0 +1,10 @@
+from sys import exit
+
+# 별 층 n 입력.
+n = int(input())
+if n < 1 or n > 100: exit(0)
+
+for i in range(1, n):
+    print(' ' * (n-i) + '*' * (2*i-1))
+for i in range(n, 0, -1):
+    print(' ' * (n-i) + '*' * (2*i-1))
