c0020746dc8b3b052dfa523eeb3195801147d4c2
Khamax <khamax4mr@gmail.com>
2023-04-21 05:08:21 +0000

    심화1 코드 수정.

--- 06 심화1/03 02444 별 찍기 -7/python/src.py
+++ 06 심화1/03 02444 별 찍기 -7/python/src.py
@@ -5,6 +5,6 @@ n = int(input())
 if n < 1 or n > 100: exit(0)
 
 for i in range(1, n):
-    print(' ' * (n-i) + '*' * (2*i-1))
+    print(f"{' ' * (n-i)}{'*' * (2*i-1)}")
 for i in range(n, 0, -1):
-    print(' ' * (n-i) + '*' * (2*i-1))
+    print(f"{' ' * (n-i)}{'*' * (2*i-1)}")
