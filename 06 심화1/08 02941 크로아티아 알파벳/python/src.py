55b4d1ba405cf591147fb7008d72341d24643cc1
Khamax <khamax4mr@gmail.com>
2023-04-25 09:50:38 +0000

    02941 python 코드, 예제, 경로, readme 추가.

---
+++ 06 심화1/08 02941 크로아티아 알파벳/python/src.py
@@ -0,0 +1,11 @@
+from sys import exit
+
+# 단어 word 입력.
+word = input()
+if len(word) > 100: exit(0)
+
+# 크로아티아 알파벳을 임의의 문자 *로 수정.
+alphabets = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+for alphabet in alphabets:
+    word = word.replace(alphabet, '*')
+print(len(word))
