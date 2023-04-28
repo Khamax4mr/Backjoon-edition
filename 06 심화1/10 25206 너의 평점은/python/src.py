08cf2c2926d504a1b7a2447dd90bfb62d225aaa8
Khamax <khamax4mr@gmail.com>
2023-04-28 13:34:42 +0000

    25206 python 코드, 예제, 경로, readme 추가.

---
+++ 06 심화1/10 25206 너의 평점은/python/src.py
@@ -0,0 +1,28 @@
+from sys import exit
+
+result_scores = []
+total_score = 0
+for _ in range(20):
+    # 과목명 name, 학점 score, 등급 grade 입력.
+    testcase = input().split()
+    name, score, grade = testcase[0], float(testcase[1]), testcase[2]
+    if len(name) < 1 or len(name) > 50: exit(0)
+    if score not in [1.0, 2.0, 3.0, 4.0]: exit(0)
+    if grade not in ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F', 'P']: exit(0)
+
+    # 학점 × 과목 평점.
+    if grade == 'P': continue
+    elif grade == 'A+': result_scores.append(score * 4.5)
+    elif grade == 'A0': result_scores.append(score * 4.0)
+    elif grade == 'B+': result_scores.append(score * 3.5)
+    elif grade == 'B0': result_scores.append(score * 3.0)
+    elif grade == 'C+': result_scores.append(score * 2.5)
+    elif grade == 'C0': result_scores.append(score * 2.0)
+    elif grade == 'D+': result_scores.append(score * 1.5)
+    elif grade == 'D0': result_scores.append(score * 1.0)
+    elif grade == 'F': result_scores.append(0)
+
+    # 학점의 총합.
+    total_score += score
+
+print(f"{(sum(result_scores) / total_score):.6f}")
