de8395f802e5009ee8dbff66ded7c6efba739f46
Khamax <khamax4mr@gmail.com>
2023-05-01 23:58:21 +0000

    10798 python 코드, 예제, 경로, readme 추가.

---
+++ 07 2차원 배열/03 10798 세로읽기/python/src.py
@@ -0,0 +1,14 @@
+from sys import exit
+
+vertical_dummy_word = ['*'] * 15 * 5
+for i in range(5):
+    # 단어 word 입력.
+    word = input()
+    if len(word) < 1 or len(word) > 15: exit(0)
+
+    for j in range(len(word)):
+        vertical_dummy_word[j*5 + i] = word[j]
+
+# 불필요한 더미 기호 제거.
+vertical_word = ''.join(map(str, vertical_dummy_word)).replace('*', '')
+print(vertical_word)
