365ced55d64760fd04d15b70d55b015088091b9e
Khamax <khamax4mr@gmail.com>
2023-05-28 01:28:04 +0000

    14215 python 코드, 예제, 경로, readme 추가.

---
+++ 10 기하: 직사각형과 삼각형/08 14215 세 막대/python/src.py
@@ -0,0 +1,21 @@
+from sys import exit
+
+# 숫자 a, b, c를 오름차순으로 정렬하는 함수.
+# return 정렬된 a, b, c 리스트.
+def sort_triangle(a, b, c):
+    sorted_list = [a, b, c]
+    sorted_list.sort()
+    return sorted_list
+
+# 세 변의 길이 a, b, c 입력.
+a, b, c = map(int, input().split())
+if a < 1 or a > 1000: exit(0)
+if b < 1 or b > 1000: exit(0)
+if c < 1 or c > 1000: exit(0)
+
+# 각 변을 오름차순 정렬하여 c가 제일 큰 수임을 가정.
+a, b, c = sort_triangle(a, b, c)
+if a + b <= c:
+    print(min(a + b, c)*2 - 1)
+else:
+    print(a + b + c)
