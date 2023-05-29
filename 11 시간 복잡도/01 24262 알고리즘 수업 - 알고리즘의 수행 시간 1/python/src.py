d2d7869e0158e4da936618e31bc0adc223c84f0a
Khamax <khamax4mr@gmail.com>
2023-05-29 03:39:35 +0000

    24262 python 코드, 예제, 경로, readme 추가.

---
+++ 11 시간 복잡도/01 24262 알고리즘 수업 - 알고리즘의 수행 시간 1/python/src.py
@@ -0,0 +1,14 @@
+from sys import exit
+
+# 입력 크기 n 입력.
+n = int(input())
+if n < 1 or n > 500000: exit(0)
+
+# MenOfPassion(A[], n) {
+#   i = ⌊n / 2⌋;
+#   return A[i]; # 코드1
+# }
+
+# 코드1 실행 횟수: 1.
+print(1)
+print(0)
