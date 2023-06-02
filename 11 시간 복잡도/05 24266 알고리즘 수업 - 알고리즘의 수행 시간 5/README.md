2fcae362551d30cbfd5001f569299913576684e2
Khamax <khamax4mr@gmail.com>
2023-06-02 08:56:37 +0000

    24266 python 코드, 예제, readme 추가.

---
+++ 11 시간 복잡도/05 24266 알고리즘 수업 - 알고리즘의 수행 시간 5/README.md
@@ -0,0 +1,9 @@
+# 알고리즘 수업 - 알고리즘의 수행 시간 5
+
+## 공통
+* [시간 복잡도 (time complexity)](https://github.com/Khamax4mr/Backjoon-edition/wiki/%EC%8B%9C%EA%B0%84-%EB%B3%B5%EC%9E%A1%EB%8F%84-(time-complexity))
+
+***
+
+## 파이썬
+* [입출력 (I/O)](https://github.com/Khamax4mr/Backjoon-edition/wiki/%EC%9E%85%EC%B6%9C%EB%A0%A5-(I-O))
---
+++ 11 시간 복잡도/05 24266 알고리즘 수업 - 알고리즘의 수행 시간 5/python/src.py
@@ -0,0 +1,16 @@
+from sys import exit
+
+# 입력 크기 n 입력.
+n = int(input())
+if n < 1 or n > 500000: exit(0)
+
+# MenOfPassion(A[], n) {
+#   sum <- 0;
+#   for i <- 1 to n
+#       for j <- 1 to n
+#           for k <- 1 to n
+#               sum <- sum + A[i] × A[j] × A[k]; # 코드1
+#   return sum;
+# }
+print(n**3)
+print(3)
