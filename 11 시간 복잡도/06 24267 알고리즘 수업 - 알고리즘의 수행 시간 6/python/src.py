2ab47fe9dbb7e7e8ef22221456c68bc5eb57cb23
Khamax <khamax4mr@gmail.com>
2023-06-03 08:57:05 +0000

    24267 python 코드, 예제, readme 추가.

---
+++ 11 시간 복잡도/06 24267 알고리즘 수업 - 알고리즘의 수행 시간 6/python/src.py
@@ -0,0 +1,16 @@
+from sys import exit
+
+# 입력 크기 n 입력.
+n = int(input())
+if n < 1 or n > 500000: exit(0)
+
+# MenOfPassion(A[], n) {
+#   sum <- 0;
+#   for i <- 1 to n - 2
+#       for j <- i + 1 to n - 1
+#           for k <- j + 1 to n
+#               sum <- sum + A[i] × A[j] × A[k]; # 코드1
+#    return sum;
+# }
+print(int((n-2)*(n-1)*n / 6))
+print(3)
