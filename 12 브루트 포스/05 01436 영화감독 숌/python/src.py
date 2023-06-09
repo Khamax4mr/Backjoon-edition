de8be09a8a27fe7cd03e67cdfd7e6dc114ae85f8
Khamax <khamax4mr@gmail.com>
2023-06-09 12:28:38 +0000

    01436 python 코드, 예제, 경로, readme 추가.

---
+++ 12 브루트 포스/05 01436 영화감독 숌/python/src.py
@@ -0,0 +1,23 @@
+from sys import exit
+
+# 영화 번호 n 입력.
+n = int(input())
+if n <= 0 or n > 10000: exit(0)
+
+numberings, i = [], 0
+while len(numberings) <= n:
+    # xxx666. (xx6666 제외)
+    if i%10 != 6:
+        numberings += [i*1000+666]
+    # xx666x. (x6666x 제외)
+    elif (i//10)%10 != 6:
+        numberings += [i*1000+660+j for j in range(10)]
+    # x666xx. (6666xx 제외)
+    elif (i//10)%100 != 66:
+        numberings += [i*1000+600+j for j in range(100)]
+    # 666xxx.
+    else:
+        numberings += [i*1000+j for j in range(1000)]
+    i += 1
+
+print(numberings[n-1])
