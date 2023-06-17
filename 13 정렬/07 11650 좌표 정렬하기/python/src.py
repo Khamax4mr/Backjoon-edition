76b9f336c9591cc6d9d87495d5cfb1113ff18079
Khamax <khamax4mr@gmail.com>
2023-06-17 00:56:21 +0000

    11650 python 코드, 예제, readme 추가.

---
+++ 13 정렬/07 11650 좌표 정렬하기/python/src.py
@@ -0,0 +1,15 @@
+from sys import exit, stdin, stdout
+
+# 점 개수 n 입력.
+n = int(input())
+if n < 1 or n > 100000: exit(0)
+
+dots = []
+for _ in range(n):
+    x, y = map(int, stdin.readline().rstrip().split())
+    if x < -100000 or x > 100000: exit(0)
+    if y < -100000 or y > 100000: exit(0)
+    dots.append((x, y))
+
+dots.sort()
+stdout.write('\n'.join([' '.join(map(str, dot)) for dot in dots]))
