63fd0157c3828642fcdf4ba68b8875f47b55f322
Khamax <khamax4mr@gmail.com>
2023-07-10 23:57:10 +0000

    07785 python 코드, 경로, 예제, readme 추가.

---
+++ 14 집합과 맵/03 07785 회사에 있는 사람/python/src.py
@@ -0,0 +1,19 @@
+from sys import exit, stdin
+
+# 출입 기록 개수 n 입력.
+n = int(input())
+if not 2 <= n <= 100000: exit()
+
+in_company = set()
+for _ in range(n):
+    # 이름 name, 출입 기록 action 입력.
+    name, action = map(str, stdin.readline().rstrip().split())
+    if len(name) > 5: exit()
+    if action not in ['enter', 'leave']: exit()
+
+    if action == 'enter':
+        in_company.add(name)
+    else:
+        in_company.remove(name)
+
+print('\n'.join(sorted(in_company, reverse=True)))
