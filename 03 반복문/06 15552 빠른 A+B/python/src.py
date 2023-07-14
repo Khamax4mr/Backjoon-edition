d68a33815ce6296a044097f64293990c519b03f3
Khamax <khamax4mr@gmail.com>
2023-07-14 11:31:11 +0000

    반복문 라이브러리 함수 선언 방식으로 수정.

--- 03 반복문/06 15552 빠른 A+B/python/src.py
+++ 03 반복문/06 15552 빠른 A+B/python/src.py
@@ -1,16 +1,18 @@
-from sys import stdin, stdout
+import sys
+readline = sys.stdin.readline
+write = sys.stdout.write
 
 # 테스트케이스 개수 t 입력.
-t = int(stdin.readline().rstrip())
+t = int(readline().rstrip())
 assert 0 < t <= 1000000
 
 sum = []
 for _ in range(t):
     # 숫자 a, b 입력.
-    a, b = map(int, stdin.readline().rstrip().split())
+    a, b = map(int, readline().rstrip().split())
     assert 1 <= a <= 1000
     assert 1 <= b <= 1000
 
     sum.append(a + b)
 
-stdout.write('\n'.join(map(str, sum)))
+write('\n'.join(map(str, sum)))
--- 03 반복문/11 10952 A+B -5/python/src.py
+++ 03 반복문/11 10952 A+B -5/python/src.py
@@ -1,6 +1,7 @@
-from sys import stdin
+import sys
+readlines = sys.stdin.readlines
 
-for line in stdin.readlines()[:-1]:
+for line in readlines()[:-1]:
     # 숫자 a, b 입력.
     a, b = map(int, line.rstrip().split())
     assert 0 < a < 10
